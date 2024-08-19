c82559e919c1899e35a09ff91d3e3cd7a1a15e7f
Vikky9135 <vikramvikky2003@gmail.com>
2024-08-19 14:43:39 +0530

    Update .env file with API key

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello Welcome !!")
+from dotenv import load_dotenv
+import os
+if __name__ == "__main__":
+    load_dotenv()
+    print("Welcome Langchain")
+    print(os.environ['COOL_API_KEY'])
